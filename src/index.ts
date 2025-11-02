de6f8b2eaf63baa92b56c6b43c83e04382036d26
manvirsingh14 <manvirtaneja07@gmail.com>
2025-11-03 00:18:32 +0530

    fixed cors

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,7 @@ configDotenv.config();
 
 const app = express();
 
-const BANK_URL = process.env.BANK_URL || "http://localhost:4000";
-const APP_URL = process.env.APP_URL || "http://localhost:3000";
-
-console.log(BANK_URL);
-console.log(APP_URL);
-
-app.use("/", () => {
-  console.log("Website has been hit");
-})
-
-app.use(cors({ origin: [BANK_URL, APP_URL] }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", webhookRouter);
